2d3a7b3571d3b0c64920c199ee9c2e74be20299b
mattg3004 <matthewgraham3004@gmail.com>
2014-07-22 15:31:02 -0400

    Corrections

--- Setup.R
+++ Setup.R
@@ -1,8 +1,5 @@
 load("~/Documents/Dengue Vietnam data/Viet_dat.RData")
-centroids1 = unlist(list.dat[1])
-centroids = matrix(0, 22, 2)
-centroids[,1] = centroids1[1:22]
-centroids[,2] = centroids1[23:44]
-incidence = unlist(list.dat[2])
-cases = unlist(list.dat[3])
-population = unlist(list.dat[4])
+centroids =  list.dat$centroids
+incidence =  list.dat$incidence
+cases     =  list.dat$cases 
+pop       =  list.dat$population
