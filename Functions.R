3be84d1509cec43cd12dabff5621279be2e76359
mattg3004 <matthewgraham3004@gmail.com>
2014-07-22 15:48:19 -0400

    f

--- Functions.R
+++ Functions.R
@@ -6,6 +6,16 @@ centroid.distance  <-  function(centroids){
       separation[i, j]  =  sqrt( (centroids[i,1] - centroids[j,1]) ^ 2  +  (centroids[i,2] - centroids[j,2]) ^ 2 )
     }
   }
-  
   return(separation)
+}
+
+gravity.model.probabilites.all.same.parameters <- function (separation, population, alpha, beta){
+  k  =  length(population)
+  flow.probs  =  matirx(0, k, k)
+  for (i in 1 : k){
+    for (j in 1 : k){
+      flow.probs[i, j] = (population[j] ^ (alpha))/ (separation[i, j] ^(beta))
+    }
+  }
+  return(flow.probs)
 }
