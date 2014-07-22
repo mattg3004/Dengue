743fc860c755999a28d0168b3181ec1cd2337a65
mattg3004 <matthewgraham3004@gmail.com>
2014-07-22 16:48:50 -0400

    Functions include method to calculate distnace in km between two points with latitude and longitude coordinates

--- Functions.R
+++ Functions.R
@@ -1,9 +1,27 @@
+require("pracma")
+
+
+
+# Calculates the geodesic distance between two points specified by radian latitude/longitude using the
+# Haversine formula (hf)
+gcd.hf <- function(long1, lat1, long2, lat2) {
+  R <- 6371 # Earth mean radius [km]
+  delta.long <- (long2 - long1)
+  delta.lat <- (lat2 - lat1)
+  a <- sin(delta.lat/2)^2 + cos(lat1) * cos(lat2) * sin(delta.long/2)^2
+  c <- 2 * asin(min(1,sqrt(a)))
+  d = R * c
+  return(d) # Distance in km
+}
+
+
 centroid.distance  <-  function(centroids){
   k  =  length(centroids[, 1])
+  centroids = deg2rad(centroids)
   separation   =   matrix(0, k, k)
   for ( i in 1 : k){
     for ( j in 1 : k){
-      separation[i, j]  =  sqrt( (centroids[i,1] - centroids[j,1]) ^ 2  +  (centroids[i,2] - centroids[j,2]) ^ 2 )
+      separation[i, j]  =  gcd.hf(centroids[i, 1], centroids[i, 2], centroids[j, 1], centroids [j, 2])
     }
   }
   return(separation)
@@ -11,11 +29,14 @@ centroid.distance  <-  function(centroids){
 
 gravity.model.probabilites.all.same.parameters <- function (separation, population, alpha, beta){
   k  =  length(population)
-  flow.probs  =  matirx(0, k, k)
+  flow.proportions  =  matrix(0, k, k)
   for (i in 1 : k){
     for (j in 1 : k){
-      flow.probs[i, j] = (population[j] ^ (alpha))/ (separation[i, j] ^(beta))
+      flow.proportions[i, j] = (population[j] ^ (alpha)) / (separation[i, j] ^(beta))
     }
+  }
+  for ( i in 1 : k){
+    flow.proportions[i, i] = 0
   }
   return(flow.probs)
 }
